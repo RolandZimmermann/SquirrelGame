f9da8da93035e18445c5632e30bec68d7e624fcf
Roland <roland@pc.de>
2018-05-04 17:12:21 +0200

    Task 4 1 & 3

---
+++ src/de/hsa/game/SquirrelGame/ui/consoletest/exceptions/NoSuchCommandException.java
@@ -0,0 +1,10 @@
+package de.hsa.game.SquirrelGame.ui.consoletest.exceptions;
+
+public class NoSuchCommandException extends ScanException {
+
+	public NoSuchCommandException(String exception) {
+		super(exception);
+		// TODO Auto-generated constructor stub
+	}
+
+}
