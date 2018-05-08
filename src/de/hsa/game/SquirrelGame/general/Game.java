e44c96ac6b2d0403b2a4c620db5af7d03a265e0e
Schneggera <top.20mai@gmail.com>
2018-05-08 15:08:47 +0200

    Teil 1 läuft.

--- src/de/hsa/game/SquirrelGame/general/Game.java
+++ src/de/hsa/game/SquirrelGame/general/Game.java
@@ -7,42 +7,84 @@ import de.hsa.game.SquirrelGame.core.board.State;
 import de.hsa.game.SquirrelGame.gamestats.MoveCommand;
 import de.hsa.game.SquirrelGame.ui.UI;
 import de.hsa.game.SquirrelGame.ui.console.ConsoleUI;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public abstract class Game {
-	private State state;
-	private UI ui;
-	private BoardView boardView;
-	private EntityContext entityContext;
+    private State state;
+    private UI ui;
+    private BoardView boardView;
+    private EntityContext entityContext;
+    private int FPS = 1;
+    private boolean multi = false;
 
-	private MoveCommand moveCommand = null;
+    private MoveCommand moveCommand = null;
 
-	public Game(State state) {
-		this.state = state;
-		this.ui = new ConsoleUI();
+    public Game(State state) {
+        this.state = state;
+        this.ui = new ConsoleUI();
 
-		FlattenBoard flattenBoard = new FlattenBoard(state.getBoard());
-		this.boardView = flattenBoard;
-		this.entityContext = flattenBoard;
-	}
+        FlattenBoard flattenBoard = new FlattenBoard(state.getBoard());
+        this.boardView = flattenBoard;
+        this.entityContext = flattenBoard;
+    }
 
-	public void run() {
-		while (true) {
-			render();
-			processInput();
-			update();
-		}
-	}
+    public void run() throws InterruptedException {
+        if (multi) {
+            Timer t = new Timer();
+            Timer m = new Timer();
+            t.schedule(new TimerTask() {
 
-	public void render() {
-		ui.render(boardView);
-	}
+                @Override
+                public void run() {
+                    while (true) {
+                        render();
+                        update();
+                        try {
+                            Thread.sleep(1000 / FPS);
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }, 1000);
 
-	public void processInput() {
-		moveCommand = ui.getCommand();
-	}
+            m.schedule(new TimerTask() {
 
-	public void update() {
-		state.update(moveCommand, entityContext);
-		boardView.update();
-	}
+                @Override
+                public void run() {
+                    while (true) {
+                        processInput();
+                        try {
+                            Thread.sleep(1000 / FPS);
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }, 1000);
+        }else {
+            while (true) {
+                render();
+                processInput();
+                update();
+            }
+        }
+
+    }
+
+    public void render() {
+        ui.render(boardView);
+    }
+
+    public void processInput() {
+        moveCommand = ui.getCommand();
+    }
+
+    public void update() {
+        state.update(moveCommand, entityContext);
+        boardView.update();
+    }
 }
