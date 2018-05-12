60583371f2c01a24a7e305c1cd253b77cf4a059f
RolandZimmermann <roland.zimmermann0@gmail.com>
2018-05-12 11:14:55 +0200

    Graphics

--- src/de/hsa/game/SquirrelGame/core/board/BoardConfig.java
+++ src/de/hsa/game/SquirrelGame/core/board/BoardConfig.java
@@ -6,10 +6,10 @@ public class BoardConfig {
 
 	
 	public static int WIDTH_SIZE = 50;
-	public static int HEIGHT_SIZE = 20;
-	public static int COUNT_WALL = 7;
+	public static int HEIGHT_SIZE = 50;
+	public static int COUNT_WALL = 10;
 	public static int COUNT_GOODBEAST = 6;
-	public static int COUNT_BADBEAST = 5;
+	public static int COUNT_BADBEAST = 6;
 	public static int COUNT_GOODPLANT = 7;
 	public static int COUNT_BADPLANT = 7;
 	public static int COUNT_HANDOPERATED_MASTERSQUIRREL = 1;
