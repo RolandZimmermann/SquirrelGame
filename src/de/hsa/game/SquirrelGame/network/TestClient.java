ffbd6fd819de155e175f971d9297e32107505171
Roland <roland@pc.de>
2018-06-20 17:54:46 +0200

    client updates

--- src/de/hsa/game/SquirrelGame/network/TestClient.java
+++ src/de/hsa/game/SquirrelGame/network/TestClient.java
@@ -7,6 +7,7 @@ import java.util.Vector;
 import de.hsa.game.SquirrelGame.network.Message.Header;
 import de.hsa.games.fatsquirrel.core.EntityType;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
@@ -20,30 +21,30 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class TestClient extends Application{
-	
+public class TestClient extends Application {
+
 	Client client = new Client("25.76.140.236", 4242);
 	Thread thread = new Thread(client);
 	Button update;
-	
+
 	private int textAreaCount;
 	private byte[][] view;
-	
+
 	public static void main(String[] args) {
-		Application.launch(args);		
-		}
+		Application.launch(args);
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Scene newScene = createScene();
 		thread.start();
-		
+
 		primaryStage.setScene(newScene);
 		primaryStage.show();
 		primaryStage.setTitle("CLIENT");
-		
+
 		System.setProperty("headless.geometry", "1600x1200-32");
-		
+
 		primaryStage.setOnCloseRequest(e -> {
 			client.setMessage(new Message(Header.CHAT, "*DISCONNECTED*"));
 			client = null;
@@ -58,13 +59,32 @@ public class TestClient extends Application{
 			@Override
 			public void run() {
 				while (true) {
-					update.fire();
+					update();
+					try {
+						Thread.sleep(1);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				}
+
 			}
+
 		}, 1000);
-		
+
 		client.setMessage(new Message(Header.CHAT, "*CONNECTED*"));
 		client.setMessage(new Message(Header.ACTION, "W"));
+	}
+
+	private void update() {
+		Platform.runLater(new Runnable() {
+
+			@Override
+			public void run() {
+				update.fire();
+
+			}
+		});
+
 	}
 
 	private Scene createScene() {
@@ -73,11 +93,11 @@ public class TestClient extends Application{
 		TextField input = new TextField();
 		Button update = new Button();
 		Label label = new Label("Chat:");
-		
+
 		Canvas canvas = new Canvas(1260, 450);
-		
+
 		this.update = update;
-		
+
 		AnchorPane.setBottomAnchor(label, 15d);
 		AnchorPane.setLeftAnchor(label, 10d);
 		AnchorPane.setLeftAnchor(input, 50d);
@@ -87,10 +107,10 @@ public class TestClient extends Application{
 		AnchorPane.setRightAnchor(canvas, 10d);
 		AnchorPane.setTopAnchor(canvas, 10d);
 		AnchorPane.setBottomAnchor(canvas, 450d);
-		root.getChildren().addAll(textArea,input,label,canvas);
-		
+		root.getChildren().addAll(textArea, input, label, canvas);
+
 		Scene scene = new Scene(root, 1280, 720);
-		
+
 		textArea.textProperty().addListener(new ChangeListener<Object>() {
 			@Override
 			public void changed(ObservableValue<?> observable, Object oldValue, Object newValue) {
@@ -98,16 +118,16 @@ public class TestClient extends Application{
 				// use Double.MIN_VALUE to scroll to the top
 			}
 		});
-		
+
 		textArea.setPrefHeight(200d);
 		textArea.setPrefWidth(500d);
 		textArea.setEditable(false);
 		textArea.setMouseTransparent(true);
 		textArea.setFocusTraversable(false);
-		
+
 		update.setOnAction(e -> {
 			Vector<String> chatMessanges = client.getChatMessage();
-			for(String s : chatMessanges) {
+			for (String s : chatMessanges) {
 				if (textAreaCount < 100) {
 					textArea.setText(textArea.getText() + "\n" + s);
 					textAreaCount++;
@@ -117,25 +137,25 @@ public class TestClient extends Application{
 				}
 			}
 			textArea.appendText("");
-			
+
 			this.view = client.getView();
-			if(this.view == null) {
+			if (this.view == null) {
 				return;
 			}
-			
+
 			GraphicsContext gc = canvas.getGraphicsContext2D();
-			
-			int CELL_HEIGHT = (int) (canvas.getHeight()/view.length);
-			int CELL_WIDTH = (int) (canvas.getWidth()/view[0].length);
-			
+
+			int CELL_HEIGHT = (int) (canvas.getHeight() / view.length);
+			int CELL_WIDTH = (int) (canvas.getWidth() / view[0].length);
+
 			gc.setFill(Color.DARKOLIVEGREEN);
 			gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-			
-			for(int y = 0; y < CELL_HEIGHT * view.length; y+=CELL_HEIGHT) {
-				for(int x = 0; x < CELL_WIDTH * view[0].length; x+=CELL_WIDTH) {
-					byte entity = view[y/CELL_HEIGHT][x/CELL_WIDTH];
+
+			for (int y = 0; y < CELL_HEIGHT * view.length; y += CELL_HEIGHT) {
+				for (int x = 0; x < CELL_WIDTH * view[0].length; x += CELL_WIDTH) {
+					byte entity = view[y / CELL_HEIGHT][x / CELL_WIDTH];
 					EntityType entityType = EntityType.NONE;
-					switch(entity) {
+					switch (entity) {
 					case 0:
 						entityType = EntityType.NONE;
 						break;
@@ -161,46 +181,46 @@ public class TestClient extends Application{
 						entityType = EntityType.MINI_SQUIRREL;
 						break;
 					}
-					
-					if(entityType == EntityType.NONE) {
+
+					if (entityType == EntityType.NONE) {
 						gc.setFill(Color.DARKOLIVEGREEN);
 						gc.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
-					} else if(entityType == EntityType.WALL) {
+					} else if (entityType == EntityType.WALL) {
 						gc.setFill(Color.ORANGE);
 						gc.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
-					} else if(entityType == EntityType.BAD_BEAST) {
+					} else if (entityType == EntityType.BAD_BEAST) {
 						gc.setFill(Color.RED);
 						gc.fillOval(x, y, CELL_WIDTH, CELL_HEIGHT);
-					} else if(entityType == EntityType.BAD_PLANT) {
+					} else if (entityType == EntityType.BAD_PLANT) {
 						gc.setFill(Color.DARKRED);
 						gc.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
-					} else if(entityType == EntityType.GOOD_BEAST) {
+					} else if (entityType == EntityType.GOOD_BEAST) {
 						gc.setFill(Color.LIGHTGREEN);
 						gc.fillOval(x, y, CELL_WIDTH, CELL_HEIGHT);
-					} else if(entityType == EntityType.GOOD_PLANT) {
+					} else if (entityType == EntityType.GOOD_PLANT) {
 						gc.setFill(Color.LIMEGREEN);
 						gc.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
-					} else if(entityType == EntityType.MASTER_SQUIRREL) {
+					} else if (entityType == EntityType.MASTER_SQUIRREL) {
 						gc.setFill(Color.HOTPINK);
 						gc.fillRect(x, y, CELL_WIDTH, CELL_HEIGHT);
-					} else if(entityType == EntityType.MINI_SQUIRREL) {
+					} else if (entityType == EntityType.MINI_SQUIRREL) {
 						gc.setFill(Color.PURPLE);
 						gc.fillOval(x, y, CELL_WIDTH, CELL_HEIGHT);
 					}
 				}
 			}
 		});
-		
+
 		input.setOnAction(e -> {
-			client.setMessage(new Message(Header.CHAT,input.getText()));
+			client.setMessage(new Message(Header.CHAT, input.getText()));
 			input.deselect();
 			root.requestFocus();
 		});
-		
+
 		scene.setOnKeyPressed(e -> {
 			client.setMessage(new Message(Header.ACTION, e.getCode().getName()));
 		});
-		
+
 		return scene;
 	}
 }
