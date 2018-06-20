cf8ac072b8b933191c2663cae5e7854c73a9f3d3
Roland <roland@pc.de>
2018-06-20 08:41:22 +0200

    more ui

--- src/de/hsa/game/SquirrelGame/mainmenu/OptionsArea.java
+++ src/de/hsa/game/SquirrelGame/mainmenu/OptionsArea.java
@@ -1,24 +1,62 @@
 package de.hsa.game.SquirrelGame.mainmenu;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import de.hsa.game.SquirrelGame.core.board.BoardConfig;
 import de.hsa.game.SquirrelGame.gamemode.GameMode;
 import javafx.collections.FXCollections;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.TextAlignment;
 
 public class OptionsArea {
 	private final AnchorPane root = new AnchorPane();
+	
+	private Label optionsLabel = new Label("Options");
+	
+	
+	private Label gameModeLabel = new Label("Select gameMode:");
 	private ChoiceBox<GameMode> gameModeBox = new ChoiceBox<GameMode>(FXCollections.observableArrayList(
 		    GameMode.JFX,GameMode.JFX3D,GameMode.MULTIPLAYER));
 	
+	private Label fpsLabel = new Label("FPS:");
+	private TextField fpsInput = new TextField(Integer.toString(BoardConfig.FPS));
+	
 	public OptionsArea() {
-		AnchorPane.setTopAnchor(gameModeBox, 100d);
+		optionsLabel.setPrefSize(60, 60);
+		optionsLabel.setTextAlignment(TextAlignment.CENTER);
 		
+		
+		AnchorPane.setTopAnchor(optionsLabel, 10d);
+		AnchorPane.setLeftAnchor(optionsLabel, 600d);
+		AnchorPane.setRightAnchor(optionsLabel, 500d);
+		
+		
+		AnchorPane.setTopAnchor(gameModeLabel, 155d);
+		AnchorPane.setLeftAnchor(gameModeLabel, 100d);
 		gameModeBox.setTooltip(new Tooltip("Select the gameMode"));
 		gameModeBox.getSelectionModel().select(GameMode.JFX);
 		
-		root.getChildren().add(gameModeBox);
+		AnchorPane.setTopAnchor(gameModeBox, 150d);
+		AnchorPane.setLeftAnchor(gameModeBox, 250d);
+		
+		AnchorPane.setTopAnchor(fpsLabel, 200d);
+		AnchorPane.setLeftAnchor(fpsLabel, 100d);
+		AnchorPane.setTopAnchor(fpsInput, 190d);
+		AnchorPane.setLeftAnchor(fpsInput, 250d);
+		
+		
+		
+		
+		root.getChildren().addAll(optionsLabel, gameModeLabel, gameModeBox, fpsLabel, fpsInput);
 	}
 	
 	
